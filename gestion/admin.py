d79dde1270b5cd461e8b7e091d8f8c0a3da254d2
ebh2024 <e.bustohurtado@itecriocuarto.org.ar>
2025-07-27 11:37:04 -0300

    Implementada gestión de pasajeros

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Vuelo, Pasajero, Reserva, Avion
 
+class ReservaInline(admin.TabularInline):
+    model = Reserva
+    extra = 1
+
+class PasajeroAdmin(admin.ModelAdmin):
+    inlines = [ReservaInline]
+
 admin.site.register(Vuelo)
-admin.site.register(Pasajero)
+admin.site.register(Pasajero, PasajeroAdmin)
 admin.site.register(Reserva)
 admin.site.register(Avion)
--- gestion/models.py
+++ gestion/models.py
@@ -23,11 +23,15 @@ class Pasajero(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
+    documento = models.CharField(max_length=20, null=True)
     email = models.EmailField()
     telefono = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+
+    def historial_vuelos(self):
+        return self.reserva_set.all()
 
 class Reserva(models.Model):
     vuelo = models.ForeignKey(Vuelo, on_delete=models.CASCADE)
