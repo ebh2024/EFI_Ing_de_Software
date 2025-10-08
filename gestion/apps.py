de5bf8fc0fd7c48eacd50c22b0874c2f91d18b2d
eb2902 <ezequielbusto987@gmail.com>
2025-10-08 09:44:35 -0300

    Reiniciar proyecto

--- gestion/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestion'
-
-    def ready(self):
-        import gestion.signals
